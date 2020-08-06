a3f53dfabba02ad3313b909e2d34881ed9689b35
junghyun <wjdguswjdal@naver.com>
2020-08-06 21:30:29 +0900

    20200806

--- src/main/java/com/grange/board/test/controller/TestController.java
+++ src/main/java/com/grange/board/test/controller/TestController.java
@@ -1,16 +1,58 @@
 package com.grange.board.test.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.grange.board.test.service.TestService;
 
 
 @RestController
+@RequestMapping(value = "/grange")
 public class TestController {
+	
+	@Autowired
+	private TestService testService;
+	
+	@RequestMapping(value ="/index")
+	public String index() {
+		return testService.hellowWorld();
+	}
+	
 	
 	@RequestMapping(value = "/test123")
 	public String test123() {
 		return "test123";
 	}
 	
+	@RequestMapping(value = "/param")
+	public String paramFromHttpServletRequest(HttpServletRequest req) {
+		
+		String param1=req.getParameter("param1");
+		String param2=req.getParameter("param2");
+		
+		System.out.println("param1 = "+param1);
+		System.out.println("param2 = "+param2);
+		return "200 ok";
+	}
+	
+	
+	@RequestMapping(value ="/param2")
+	public String paramFromRequestParam(@RequestParam(name = "param1",required=true) String param1,@RequestParam(name = "param2",required=true) String param2) {
+
+		System.out.println("param1 = "+param1);
+		System.out.println("param2 = "+param2);
+		
+		return "200 ok";
+	}
+	
+	@RequestMapping(value="/add")
+	public int adder(@RequestParam(name="operand1") int operand1,@RequestParam(name="operand2")int operand2) {
+		int result=0;
+		
+		return operand1+operand2;
+	}
 }
--- src/main/java/com/grange/board/test/service/TestService.java
+++ src/main/java/com/grange/board/test/service/TestService.java
@@ -3,8 +3,12 @@ package com.grange.board.test.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.grange.board.test.dao.TestDao;
+
 @Service
 public class TestService {
+	//@Autowired
+	private TestDao testDao;
 	
 	public String hellowWorld() {
 		return "Hello World";

b15de910a7c18972f4d5085fe41982b0a6a926d8
wjdgus95 <wjdguswjdal@naver.com>
2020-12-18 22:30:29 +0900

    20201218

--- src/test/java/com/grange/board/GrangeBoardApplicationTests.java
+++ src/test/java/com/grange/board/GrangeBoardApplicationTests.java
@@ -1,28 +1,45 @@
 package com.grange.board;
 
+import java.util.List;
+
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.grange.board.board.dao.BoardDao;
+import com.grange.board.board.service.BoardService;
 import com.grange.board.board.vo.BoardVO;
+import com.grange.board.comment.dao.CommentDao;
+import com.grange.board.comment.vo.CommentVO;
+
+
 
 @SpringBootTest
 class GrangeBoardApplicationTests {
 
-//	@Autowired
-//	private BoardService boardService;
+	@Autowired
+	private BoardDao boardDao;
+	
+	@Autowired
+	private CommentDao commentDao;
 	
 	@Test
 	void contextLoads() {
 //		BoardVO board=new BoardVO();
 //		board.setTitle(title);
 		
-		String pw="wjdguswjdal";
+		/*String pw="wjdguswjdal";
 		System.out.println("origin"+pw);
 		
 		pw=DigestUtils.sha256Hex(pw);
-		System.out.println(pw);
+		System.out.println(pw);*/
+		
+		int boardId = 65598;
+		
+		List<CommentVO> comments = this.commentDao.getComments(boardId);
+		System.out.println(comments.toString());
+		
 	}
 	
 
